8c500f448dccca43419486c82cd8b476b1bdf1c6
Yusuke Shirota <y.shiro9102@gmail.com>
2020-07-23 01:59:19 +0900

    git

--- Pods/Target Support Files/Pods-tableview_ios-tableview_iosUITests/Pods-tableview_ios-tableview_iosUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tableview_ios_tableview_iosUITests : NSObject
-@end
-@implementation PodsDummy_Pods_tableview_ios_tableview_iosUITests
-@end
--- Pods/Target Support Files/Pods-tableview_iosTests/Pods-tableview_iosTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tableview_iosTests : NSObject
-@end
-@implementation PodsDummy_Pods_tableview_iosTests
-@end
